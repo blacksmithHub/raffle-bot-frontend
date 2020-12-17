6830663efebae85e43f9113fcd025a5295a850f4
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2020-12-17 13:35:28 +0800

    Initial Commit

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack')
+
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          PACKAGE_JSON: '"' + escape(JSON.stringify(require('./package.json'))) + '"'
+        }
+      })
+    ]
+  }
+}
